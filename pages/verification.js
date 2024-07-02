62c8480e34f14c343688418fa1b5635790aa129e
Votre nom <votre-email@example.com>
2024-07-03 01:18:06 +0200

    Déploiement ok

--- pages/verification.js
+++ pages/verification.js
@@ -1,12 +1,14 @@
 // pages/verification.js
+
+
 import { useState } from "react";
 import { useRouter } from "next/router";
 import { sendEmailVerification } from "firebase/auth";
 import { auth } from "../config/firebase";
-import styles from "../app/styles/verification.css"; 
+import styles from "../app/styles/verification.css";
 
 export default function Verification() {
-  const [email, setEmail] = useState("");
+  const [code, setCode] = useState("");
   const [verificationError, setVerificationError] = useState(null);
   const [verificationSuccess, setVerificationSuccess] = useState(false);
   const router = useRouter();
@@ -15,12 +17,12 @@ export default function Verification() {
     try {
       const user = auth.currentUser;
 
-      if (user && user.email === email) {
+      if (user) {
         await sendEmailVerification(user);
         console.log("Verification email sent.");
         setVerificationSuccess(true);
       } else {
-        setVerificationError("Email does not match with the signed-in user.");
+        setVerificationError("No user is signed in.");
       }
     } catch (error) {
       console.error("Error sending verification email:", error);
@@ -30,27 +32,23 @@ export default function Verification() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.verificationBox}>
-        <h1>Vérification de l'e-mail</h1>
-        <p>Veuillez saisir votre adresse e-mail pour recevoir un lien de vérification.</p>
-        <div className={styles.inputContainer}>
-          <input
-            type="email"
-            placeholder="Votre adresse e-mail"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className={styles.inputField}
-          />
-        </div>
-        <div className={styles.submitButton}>
-          <button type="button" onClick={handleEmailVerification}>Envoyer le lien de vérification</button>
-        </div>
-        {verificationError && <p className={styles.errorMessage}>{verificationError}</p>}
-        {verificationSuccess && (
-          <p className={styles.successMessage}>Un email de vérification a été envoyé ! Veuillez vérifier votre boîte de réception.</p>
-        )}
-      </div>
+      <h1>vérification mail</h1>
+      <p>Checker ta boite mail pour valider ton compte</p>
+      <input
+        type="text"
+        placeholder="Code de validation"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+        className={styles.inputField}
+      />
+      <button onClick={handleEmailVerification} className={styles.submitButton}>
+        Valider
+      </button>
+      <button className={styles.resendButton}>
+        Renvoyer le code
+      </button>
+      {verificationError && <p className={styles.errorMessage}>{verificationError}</p>}
+      {verificationSuccess && <p className={styles.successMessage}>Un email de vérification a été envoyé ! Veuillez vérifier votre boîte de réception.</p>}
     </div>
   );
 }
