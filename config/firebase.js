4eaf490b29f3f0be22f06ba673ab8d8148812c7c
Votre nom <votre-email@example.com>
2024-07-02 23:12:14 +0200

    Déploiement ok

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,6 @@
 // firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth,  GoogleAuthProvider  } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -16,7 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-
-// Initialisez GoogleAuthProvider si nécessaire
 const googleProvider = new GoogleAuthProvider();
+
+
 export { auth, db, googleProvider };
